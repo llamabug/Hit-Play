04003c0b0d6ad05b4e6639f79c6ad9b38a52742e
llamabug <matildajaprea@gmail.com>
2026-04-04 19:49:12 +1300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function search() {
+    var input = document.getElementById("search").value.toLowerCase().trim();
+
+    if (input === 'happy') {
+        window.location.href = "happy.html"
+    }
+}
