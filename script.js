4bb0c1257f7aa8ad52e03a3e404953c188ae3bec
llamabug <matildajaprea@gmail.com>
2026-04-05 13:06:26 +1200

    Hour 5 update- script.js

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 function search() {
     var input = document.getElementById("search").value.toLowerCase().trim();
 
-    if (input === 'happy') {
-        window.location.href = "happy.html"
+    if (input === 'mad') {
+        window.location.href = "red.html"
+    }
+
+    else if (input === 'party') {
+        window.location.href = "orange.html"
+    }
+
+    else if (input === 'happy') {
+        window.location.href = "yellow.html"
+    }
+
+    else if (input === 'focus') {
+        window.location.href = "green.html"
+    }
+
+    else if (input === 'sad') {
+        window.location.href = "blue.html"
+    }
+
+    else if (input === 'confident') {
+        window.location.href = "purple.html"
+    }
+
+    else if (input === 'love') {
+        window.location.href = "pink.html"
     }
 
     else {
         alert('Mood not recognised');
     }
 }
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    // Replace with your specific action
+    search()
+  }
+});
